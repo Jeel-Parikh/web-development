7748872ea15424a7811bc62a4803711795de8855
Jeel <Jeel-Parikh>
2022-06-20 23:55:02 +0530

    single chnage

--- CRUD in mongo/index.js
+++ CRUD in mongo/index.js
@@ -3,15 +3,12 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require("path");
 const methodOverride = require("method-override");
-
 const AppError = require("./AppError");
-
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")))
-
 const People = require("./product schema.js");
 const appError = require("./appError.js");
 mongoose.connect('mongodb://localhost:27017/people')
